8806cd5c263da4cf15d9764866664fa86b4642ad
Abhinav Mudugu <abhinav.mudugu@gmail.com>
2025-10-01 01:08:24 +0000

    added change user_name and user_email fields

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -28,7 +28,7 @@ pub use crate::backend::authenticate::*;
 pub mod add_to_list;
 pub use crate::backend::add_to_list::add_anime_to_list;
 use crate::backend::sign_up::check_username_availability;
-use crate::backend::user_profile::{change_pfp, get_user_details, logout};
+use crate::backend::user_profile::{change_email, change_pfp, change_username, get_user_details, logout};
 
 pub mod user_profile;
 
@@ -144,6 +144,8 @@ pub async fn setup_backend() -> std::io::Result<()> {
             .service(get_user_details)
             .service(logout)
             .service(change_pfp)
+            .service(change_username)
+            .service(change_email)
     }).bind("127.0.0.1:3000")?
     .run()
     .await
--- src/frontend/manage_user_profile.rs
+++ src/frontend/manage_user_profile.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 use crate::frontend::title_bar::UserDetails;
 pub use crate::frontend::*;
+use dioxus::desktop::tao::event;
 use rfd::AsyncFileDialog;
 pub use anyhow::Result;
 use serde_json::json;
@@ -10,16 +11,35 @@ pub struct ChangePfp{
     pfp: String,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct ChangeUsername{
+    user_name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChangePassword{
+    pwd: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChangeEmail{
+    email: String,
+}
+
 #[component]
 pub fn ManageAccount() -> Element{
     let mut username = use_signal(|| "".to_string());
     let mut user_email = use_signal(|| "".to_string());
     let mut user_pfp = use_signal(|| "".to_string());
-    let mut refetch_signal = use_signal(|| false);
     let mut refetch_title = use_context::<Signal<bool>>();
 
+    let mut edit_username_trigger = use_signal(|| true);
+    let mut edit_email_trigger = use_signal(|| true);
+    let mut edit_pwd_trigger = use_signal(|| true);
+
     use_effect(move || {
-        let mut _a = refetch_signal.read();
+        //let mut _a = refetch_signal.read();
+        let _ = refetch_title.read();
         let user_name = USERNAME.cloned();
         let user_id = USERID.cloned();
         if user_id != -1 && user_name != "".to_string(){ 
@@ -37,7 +57,60 @@ pub fn ManageAccount() -> Element{
             });
         }}
     );
-    
+
+    let mut submit_username = {
+        let username = username.clone();
+        let mut refetch_title = refetch_title.clone();
+
+        move || {
+            edit_username_trigger.set(true); // lock field again
+
+            spawn(async move {
+                let client = Client::new();
+                if let Ok(_res) = client
+                    .post("http://localhost:3000/change_username")
+                    .json(&ChangeUsername {
+                        user_name: username.read().to_string(),
+                    })
+                    .bearer_auth(TOKEN.read())
+                    .send()
+                    .await
+                {
+                    dbg!("Username changed (via submit)");
+                    let current = !*refetch_title.read();
+                    refetch_title.set(current);
+                }
+            });
+        }
+    };
+
+    let mut submit_email = {
+        let email = user_email.clone();
+        let mut refetch_title = refetch_title.clone();
+
+        move || {
+            edit_email_trigger.set(true); // lock field again
+
+            spawn(async move {
+                let client = Client::new();
+                if let Ok(_res) = client
+                    .post("http://localhost:3000/change_email")
+                    .json(&ChangeEmail {
+                        email: email.read().to_string(),
+                    })
+                    .bearer_auth(TOKEN.read())
+                    .send()
+                    .await
+                {
+                    dbg!("email changed (via submit)");
+                    let current = !*refetch_title.read();
+                    refetch_title.set(current);
+                }
+            });
+        }
+    };
+
+
     rsx!(
 
         div { 
@@ -52,10 +125,8 @@ pub fn ManageAccount() -> Element{
                             }
                         )
                         .bearer_auth(TOKEN.read()).send().await {
-                            let current_refresh = *refetch_signal.read();
-                            refetch_signal.set(!current_refresh);
-                            // add some actuall error handeling
-                            let mut current = !*refetch_title.read();
+                            //let current_refresh = *refetch_signal.read();
+                            let current = !*refetch_title.read();
                             refetch_title.set(current);
                         }
                     }
@@ -66,6 +137,81 @@ pub fn ManageAccount() -> Element{
                 src: user_pfp,
                 alt: "user_image"
             },
+        }
+
+        div { 
+            id:"User_details_div",
+            h2 { "Account Details:" }
+
+            div { 
+                id: "Username_div",
+                label { "Username:" }
+                input { 
+                    class:"change_details_div",
+                    r#type: "text",
+                    value: "{ username }",
+                    disabled: edit_username_trigger, // change so only disable till the user clcicks the button
+                    oninput: move |evt| {
+                        username.set(evt.value());
+                    },
+                    onkeydown: move |evt| {
+                        if evt.code().to_string() == "Enter".to_string() {
+                            submit_username();
+                        } 
+                    }
+                }
+                
+                if *edit_username_trigger.read(){
+                    button { 
+                        class: "confirm_change_buttons",
+                        onclick: move |_| {
+                            edit_username_trigger.set(false);
+                        },
+                        "Edit"
+                    }
+                } else {
+                    button { 
+                        class: "confirm_change_buttons",
+                        onclick: move |_| submit_username(),
+                        "Submit"
+                    }
+                }
+            }
+
+            div { 
+                id: "email_div",
+                label { "Email:" }
+                input { 
+                    class:"change_details_div",
+                    r#type: "text",
+                    value: "{ user_email }",
+                    disabled: edit_email_trigger, // change so only disable till the user clcicks the button
+                    oninput: move |evt| {
+                        user_email.set(evt.value());
+                    },
+                    onkeydown: move |evt| {
+                        if evt.code().to_string() == "Enter".to_string() {
+                            submit_email();
+                        } 
+                    }
+                }
+                
+                if *edit_username_trigger.read(){
+                    button { 
+                        class: "confirm_change_buttons",
+                        onclick: move |_| {
+                            edit_email_trigger.set(false);
+                        },
+                        "Edit"
+                    }
+                } else {
+                    button { 
+                        class: "confirm_change_buttons",
+                        onclick: move |_| submit_email(),
+                        "Submit"
+                    }
+                }
+            }
          }
     )
 }
